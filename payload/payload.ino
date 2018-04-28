77019f1f91e77553a1648c56ff214f1bd748caaa
skyhoffert <skwylarh@yahoo.com>
2018-04-28 15:06:24 -0400

    Uploading example program.

---
+++ payload/payload.ino
@@ -0,0 +1,26 @@
+// VTCanSat
+// payload.ino
+// Main program for CanSat 2018 competition
+
+
+
+// util functions ------------------------------------------------------------------------------
+// adds two given integers // example
+int add( int a, int b ){ // example
+  return a + b; // example
+} // example
+
+// constants -----------------------------------------------------------------------------------
+float ANSWER_TO_ALL = 42; // example
+
+// variables -----------------------------------------------------------------------------------
+int pin_GPS = -1; // example
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
